5373044dbbed4cf8a6d0bd78d011582f6f552970
jpliu <fortune_coder@foxmail.com>
2022-03-23 22:32:29 +0800

    feat: support useHttp hooks method

--- src/hooks-api.ts
+++ src/hooks-api.ts
@@ -1,9 +1,59 @@
-import type { AxiosResponse } from 'axios'
-import type { JPRequestConfig } from './jp-axios'
+
+import type { AxiosInstance, AxiosResponse } from 'axios'
+import type { JPInterceptors, JPRequestConfig } from './jp-axios'
 import { JPAxios } from './jp-axios'
+
+type Method = JPRequestConfig['method']
+
+interface UseHttp {
+  <T = AxiosResponse>(config: JPRequestConfig<T>): Promise<T>
+  <T = AxiosResponse>(url: string, config?: Omit<JPRequestConfig, 'url'>): Promise<T>
+  <T = AxiosResponse>(url: string, method: Method, config?: Omit<JPRequestConfig, 'url'| 'method'>): Promise<T>
+  instance: AxiosInstance
+  get<U>(config: JPRequestConfig<U>): Promise<U>
+  post<U>(config: JPRequestConfig<U>): Promise<U>
+  put<U>(config: JPRequestConfig<U>): Promise<U>
+  delete<U>(config: JPRequestConfig<U>): Promise<U>
+  head<U>(config: JPRequestConfig<U>): Promise<U>
+  patch<U>(config: JPRequestConfig<U>): Promise<U>
+  options<U>(config: JPRequestConfig<U>): Promise<U>
+  use(interceptors: JPInterceptors): void
+}
 
 const jpAxiosInstance = new JPAxios()
 
-export function useHttp<T = AxiosResponse>(config: JPRequestConfig<T>) {
-  return jpAxiosInstance.request<T>(config)
+export const useHttp: UseHttp = function us<T>(
+  urlOrConfig: any,
+  methodOrConfig?: any,
+  config?: any
+): Promise<T> {
+  const len = arguments.length
+  if (len === 1) {
+    return jpAxiosInstance.request<T>(urlOrConfig)
+  }
+  else if (len === 2) {
+    return jpAxiosInstance.request<T>({
+      url: urlOrConfig,
+      ...(methodOrConfig as any),
+    })
+  }
+  else {
+    return jpAxiosInstance.request<T>({
+      url: urlOrConfig,
+      methodOrConfig,
+      ...(config as any)
+    })
+  }
 }
+
+useHttp.instance = jpAxiosInstance.instance
+useHttp.get = jpAxiosInstance.get.bind(jpAxiosInstance)
+useHttp.post = jpAxiosInstance.post.bind(jpAxiosInstance)
+useHttp.put = jpAxiosInstance.put.bind(jpAxiosInstance)
+useHttp.delete = jpAxiosInstance.delete.bind(jpAxiosInstance)
+useHttp.head = jpAxiosInstance.head.bind(jpAxiosInstance)
+useHttp.patch = jpAxiosInstance.patch.bind(jpAxiosInstance)
+useHttp.options = jpAxiosInstance.options.bind(jpAxiosInstance)
+useHttp.use = jpAxiosInstance.use.bind(jpAxiosInstance)
+
+export default useHttp
--- test/hooks-api.spec.ts
+++ test/hooks-api.spec.ts
@@ -3,8 +3,83 @@ import { useHttp } from '../src'
 
 describe('jp-axios hooks api', () => {
   test('happy path, useHttp', async() => {
-    const res = await useHttp<{ data: { name: string; age: number; height: number } }>({ url: 'http://localhost:3000/user/info', method: 'get' })
-    expect(res.data).toMatchObject({
+    const res1 = await useHttp<{ data: { name: string; age: number; height: number } }>('http://localhost:3000/user/info', 'GET')
+    const res2 = await useHttp<{ data: { name: string; age: number; height: number } }>({ url: 'http://localhost:3000/user/info', method: 'get' })
+    const res3 = await useHttp.get<{ data: { name: string; age: number; height: number } }>({ url: 'http://localhost:3000/user/info' })
+    expect(res1.data).toMatchObject({
+      name: '小明',
+      age: 18,
+      height: 1.88
+    })
+    expect(res2.data).toMatchObject({
+      name: '小明',
+      age: 18,
+      height: 1.88
+    })
+    expect(res3.data).toMatchObject({
+      name: '小明',
+      age: 18,
+      height: 1.88
+    })
+  })
+  test('useHttp hooks interface interceptor', async() => {
+    const reqArr: string[] = []
+    const resArr: string[] = []
+    const res1 = await useHttp<{ data: { name: string; age: number; height: number } }>(
+      'http://localhost:3000/user/info',
+      'GET',
+      {
+        interceptors: {
+          requestInterceptor(config) {
+            reqArr.push('first request')
+            return config
+          },
+          responseInterceptor(res) {
+            resArr.push('first response')
+            return res.data
+          }
+        }
+      })
+    expect(reqArr).toMatchObject(['first request'])
+    expect(resArr).toMatchObject(['first response'])
+    expect(res1).toMatchObject({
+      name: '小明',
+      age: 18,
+      height: 1.88
+    })
+  })
+
+  test('useHttp hooks interceptor', async() => {
+    const reqArr: string[] = []
+    const resArr: string[] = []
+    useHttp.use({
+      requestInterceptor(config) {
+        reqArr.push('first request')
+        return config
+      },
+      responseInterceptor(res) {
+        resArr.push('first response')
+        return res.data
+      }
+    })
+    const res1 = await useHttp<{ data: { name: string; age: number; height: number } }>(
+      'http://localhost:3000/user/info',
+      'GET',
+      {
+        interceptors: {
+          requestInterceptor(config) {
+            reqArr.push('second request')
+            return config
+          },
+          responseInterceptor(res) {
+            resArr.push('second response')
+            return res
+          }
+        }
+      })
+    expect(reqArr).toMatchObject(['second request', 'first request'])
+    expect(resArr).toMatchObject(['first response', 'second response'])
+    expect(res1).toMatchObject({
       name: '小明',
       age: 18,
       height: 1.88
