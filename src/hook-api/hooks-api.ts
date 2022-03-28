71291d865aed8fd34b1cf0ae54345f41fe0a51e2
jpliu6 <jpliu6@iflytek.com>
2022-03-28 12:35:42 +0800

    chore: update hook api for interceptor generic

--- src/hook-api/hooks-api.ts
+++ src/hook-api/hooks-api.ts
@@ -1,7 +1,7 @@
 
 import type { AxiosInstance, AxiosResponse } from 'axios'
 import { JPAxios } from '../jp-axios/jp-axios'
-import type { JPInterceptors, JPRequestConfig } from '../jp-axios/jp-axios'
+import type { JPRequestConfig } from '../jp-axios/jp-axios'
 
 type Method = JPRequestConfig['method']
 
@@ -11,19 +11,19 @@ interface UseHttp {
   <T = AxiosResponse>(url: string, config?: Omit<JPRequestConfig, 'url'>): Promise<T>
   <T = AxiosResponse>(url: string, method: Method, config?: Omit<JPRequestConfig, 'url'| 'method'>): Promise<T>
   instance: AxiosInstance
-  get<U>(config: JPRequestConfig<U>): Promise<U>
-  post<U>(config: JPRequestConfig<U>): Promise<U>
-  put<U>(config: JPRequestConfig<U>): Promise<U>
-  delete<U>(config: JPRequestConfig<U>): Promise<U>
-  head<U>(config: JPRequestConfig<U>): Promise<U>
-  patch<U>(config: JPRequestConfig<U>): Promise<U>
-  options<U>(config: JPRequestConfig<U>): Promise<U>
-  use(interceptors: JPInterceptors): void
+  get: typeof jpAxiosInstance.get
+  post: typeof jpAxiosInstance.post
+  put: typeof jpAxiosInstance.put
+  delete: typeof jpAxiosInstance.delete
+  head: typeof jpAxiosInstance.head
+  patch: typeof jpAxiosInstance.patch
+  options: typeof jpAxiosInstance.options
+  use: typeof jpAxiosInstance.use
 }
 
 const jpAxiosInstance = new JPAxios()
 
-export const useHttp: UseHttp = function us<T>(
+export const useHttp: UseHttp = function useHttp<T>(
   urlOrConfig: any,
   methodOrConfig?: any,
   config?: any
@@ -31,7 +31,7 @@ export const useHttp: UseHttp = function us<T>(
   const len = arguments.length
   if (len === 1) {
     if (typeof urlOrConfig === 'string')
-      return jpAxiosInstance.request({ url: urlOrConfig })
+      return jpAxiosInstance.request<T>({ url: urlOrConfig })
 
     return jpAxiosInstance.request<T>(urlOrConfig)
   }
