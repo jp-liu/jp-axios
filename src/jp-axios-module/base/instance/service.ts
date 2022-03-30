2f671756c88e719687a1833e191e3cab63068515
jpliu6 <jpliu6@iflytek.com>
2022-03-30 18:48:25 +0800

    feat: add base template for generate module api

---
+++ src/jp-axios-module/base/instance/service.ts
@@ -0,0 +1,27 @@
+import JPAxios from '../../..'
+import {
+  requestInterceptor,
+  requestInterceptorCatch,
+  responseInterceptor,
+  responseInterceptorCatch,
+} from '../interceptor/interceptor'
+
+/**
+ * @description 导出实例
+ */
+export const instance = new JPAxios()
+
+/**
+ * @description 注册第一组拦截器
+ */
+instance.use({
+  requestInterceptor,
+  requestInterceptorCatch,
+  responseInterceptor,
+  responseInterceptorCatch,
+})
+
+/**
+ * @description 导出实例通用请求方法
+ */
+export const request = instance.request
