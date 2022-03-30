409bdeb4847729dfebe02f6653637ad51b94477e
jpliu <fortune_coder@foxmail.com>
2022-03-30 23:38:37 +0800

    fix: change template and generate code

---
+++ src/jp-axios-module/index.ts
@@ -0,0 +1,15 @@
+
+import { resolve } from 'path'
+import { generateApi } from 'swagger-typescript-api'
+generateApi({
+  modular: true,
+  input: resolve(__dirname, './swagger.json'),
+  output: resolve(__dirname, './module'),
+  extractRequestParams: true,
+  // because this script was called from package.json folder
+  templates: './src/jp-axios-module/templates/eta',
+})
+  .then(() => {
+    // 1.将`base`文件夹内的内容拷贝到`outputPath`文件夹内
+    // 2.将`module`文件夹的内容剪切到`outputPath`文件夹内
+  })
