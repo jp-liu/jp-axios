527808ac51f6b082d01672b8f89875ce8f177ee7
jpliu <fortune_coder@foxmail.com>
2022-04-03 10:10:39 +0800

    fix: change module api directory name

--- src/jp-axios-module/index.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path'
-import { generateModuleApi } from './generate'
-
-const input = path.resolve(__dirname, './test/swagger.json')
-const output = path.resolve(__dirname, './test/module')
-
-generateModuleApi({ input, output })
--- src/jp-axios-module/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './files'
-export * from './renderTemplate'
-export * from './removeHeadComment'
