27691830b2f55c4c1187f8052d2e987376c395b7
jpliu6 <jpliu6@iflytek.com>
2022-04-06 12:53:20 +0800

    fix(api): change cli exec fn

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,12 @@
 import { getGenerateModuleConfig } from './config'
 import { generateModule } from './generate'
+import type { GenerateConfig } from './types'
 
-const config = getGenerateModuleConfig()
-generateModule(config)
+/**
+ * @description 开始生成模块`api`
+ * @param config 可以自行获取并调用
+ */
+export function generate(config?: GenerateConfig) {
+  config = config || getGenerateModuleConfig()
+  generateModule(config)
+}
