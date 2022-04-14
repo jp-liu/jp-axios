88cc69d16ca49b133ab406847e548451681012af
jpliu <fortune_coder@foxmail.com>
2022-04-14 23:51:16 +0800

    feat(api): support response not unpacking

--- packages/api/src/utils/wrapResponse.ts
+++ packages/api/src/utils/wrapResponse.ts
@@ -3,7 +3,8 @@ import type { GenerateContext } from '../types'
 import { getFileContent, writeFile } from './files'
 
 export function wrapResponse(context: GenerateContext) {
-  const { output } = context
+  const { output, unwrapResponse, useAxios } = context
+  if (unwrapResponse && useAxios) return
   const instancePath = resolve(output as string, './instance/index.ts')
   const content = getFileContent(instancePath)
   writeFile(instancePath, content.replace('.then(res => res.data)', ''))
