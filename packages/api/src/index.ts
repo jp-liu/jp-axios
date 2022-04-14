f7f79124e812130b70ff16bbfaf29194a5aab125
jpliu <fortune_coder@foxmail.com>
2022-04-14 23:47:15 +0800

    fix(api): restricted program entry type

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -6,7 +6,10 @@ import type { GenerateConfig } from './types'
  * @description 开始生成模块`api`
  * @param config 可以自行获取并调用
  */
-export function generate(config?: GenerateConfig) {
-  config = config || getGenerateModuleConfig()
+export function generate(config?: Omit<GenerateConfig, 'input' | 'url'>): void
+export function generate(config?: Omit<GenerateConfig, 'input' | 'spec'>): void
+export function generate(config?: Omit<GenerateConfig, 'url' | 'spec'>): void
+export function generate(config?: any) {
+  config = getGenerateModuleConfig(config)
   generateModule(config)
 }
